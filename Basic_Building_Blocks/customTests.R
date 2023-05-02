9c5b83e41eb93abd7b3be03ceb726a6b1c64cb5d
rebdau <100350212+rebdau@users.noreply.github.com>
2023-05-02 13:47:12 +0200

    Update customTests.R

--- Basic_Building_Blocks/customTests.R
+++ Basic_Building_Blocks/customTests.R
@@ -27,7 +27,7 @@ submit_log <- function(){
   
   # Please edit the link below
   # pre_fill_link <- "https://docs.google.com/forms/d/1ngWrz5A5w5RiNSuqzdotxkzgk0DKU-88FmnTHj20nuI/viewform?entry.1733728592"
-  pre_fill_link <- "https://docs.google.com/forms/d/e/1FAIpQLSdAOjg2BmNt7qtlj-KZbdoq7Gk_6uvZEy3xh0OBhqvqwBwjhQ/viewform?usp=pp_url&entry.419798233=1"
+  pre_fill_link <- "https://docs.google.com/forms/d/e/1FAIpQLSdAOjg2BmNt7qtlj-KZbdoq7Gk_6uvZEy3xh0OBhqvqwBwjhQ/viewform?entry.419798233"
   # Do not edit the code below
   if(!grepl("=$", pre_fill_link)){
     pre_fill_link <- paste0(pre_fill_link, "=")
